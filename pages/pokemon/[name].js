a7cf4a43ef67e0774d88393b3c5b37c1157cb911
Muna Nadhifa Ridwan <mnadhifa@prixa.ai>
2024-04-24 15:32:48 +0700

    update detail page

--- pages/pokemon/[name].js
+++ pages/pokemon/[name].js
@@ -9,6 +9,28 @@ function PokemonPage() {
   const router = useRouter();
   const { name } = router.query;
   const [pokemonData, setPokemonData] = useState(null);
+  const [speciesData, setSpeciesData] = useState(null);
+
+  const colors = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
+  };
 
   useEffect(() => {
     console.log("router query:", router.query);
@@ -29,9 +51,31 @@ function PokemonPage() {
     fetchPokemonDetails();
   }, [name, router.query]);
 
-  if (!pokemonData) {
+  useEffect(() => {
+    if (!pokemonData) return;
+
+    async function fetchSpeciesDetails() {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon-species/${pokemonData.id}/`
+        );
+        const data = await response.json();
+        setSpeciesData(data);
+      } catch (error) {
+        console.error("Error fetching Pokémon species:", error);
+      }
+    }
+
+    fetchSpeciesDetails();
+  }, [pokemonData]);
+
+  if (!pokemonData || !speciesData) {
     return <div>Loading...</div>;
   }
+
+  const speciesDescription = speciesData.flavor_text_entries.find(
+    (entry) => entry.language.name === "en"
+  ).flavor_text;
 
   return (
     <main className="min-h-screen bg-fuchsia-400 p-20 font-mono">
@@ -55,7 +99,7 @@ function PokemonPage() {
                 alt={pokemonData.name}
                 className="w-7 h-7 mr-10"
               />
-              <h1 className="text-lg mt-0"> No {pokemonData.id} </h1>
+              <h1 className="text-lg mt-0"> No {speciesData.id} </h1>
             </div>
             <div className=" px-4 py-5 sm:px-6 bg-black rounded-l-sm text-lg w-52">
               {pokemonData.name}
@@ -67,8 +111,51 @@ function PokemonPage() {
               <h1>Detail Pokémon</h1>
             </div>
 
-            <div className="mt-1 px-3 py-3 sm:px-5 bg-gray-200 flex rounded-r-sm text-black place-content-center">
-              <h1>Detail Pokémon</h1>
+            <div className="mt-1 px-3 py-3 sm:px-5 bg-gray-200  rounded-r-sm text-black place-content-center divide-y divide-neutral-400">
+              <div className="flex">
+                <h1 className="mr-[100px]">Types</h1>
+                <div className="flex ">
+                  {pokemonData.types.map((type, typeIndex) => (
+                    <p
+                      key={typeIndex}
+                      className=" mr-[20px] w-[100px] h-[25px] p-1  text-center text-xs text-white rounded overflow-hidden shadow-lg max-w-sm"
+                      style={{
+                        backgroundColor: colors[type.type.name],
+                      }}
+                    >
+                      {type.type.name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+
+              <div className="flex mt-[15px] pt-[15px] ">
+                <h1 className="mr-[100px]">Height</h1>
+                <p>{pokemonData.height}</p>
+              </div>
+
+              <div className="flex mt-[15px] pt-[15px] ">
+                <h1 className="mr-[100px]">Weight</h1>
+                <p>{pokemonData.weight}</p>
+              </div>
+
+              <div className="flex mt-[15px] pt-[15px] ">
+                <h1 className="mr-[100px]">Abilities</h1>
+                <div className="flex ml-[-40px]">
+                  {pokemonData.abilities.map((ability, index) => (
+                    <p
+                      key={index}
+                      className="mr-4 px-2 py-1 text-xs text-white bg-gray-500 rounded"
+                    >
+                      {ability.ability.name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            <div className="px-3 mt-1 py-3 sm:px-5 bg-gray-200 flex rounded-r-sm text-black place-content-center">
+              <p className="w-[400px]">{speciesDescription}</p>
             </div>
           </div>
         </div>
